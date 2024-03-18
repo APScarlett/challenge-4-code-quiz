bdede16775d4afb12a7a97c7869f84f638fb997a
Alexander Scarlett <ascarlett92@yahoo.com>
2024-03-18 16:11:11 -0400

    Added multiple variables in javascript as well as a function and callback

---
+++ Assets/js/script.js
@@ -0,0 +1,13 @@
+var introEl= document.getElementById("intro")
+var questionSectionEl=document.getElementById("question-section")
+var initialInputEl= document.getElementById("intial-input")
+var highscoreEl= customElement.getElementById("highscore")
+
+var startQuizEl= document.getElementById("start-quiz")
+var questionTitleEl= document.getElementById("question-title")
+var choiceListEl= document.getElementById("choice-list")
+
+function startQuiz(){
+
+}
+startQuizEl.addEventListener("click", startQuiz) //callback for click
