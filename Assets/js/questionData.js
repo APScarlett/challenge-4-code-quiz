43f4e4d9ce027a87d3e9c304ae98802a519e2d91
Alexander Scarlett <ascarlett92@yahoo.com>
2024-03-18 16:50:32 -0400

    Added questiondata js file and added timer

---
+++ Assets/js/questionData.js
@@ -0,0 +1,28 @@
+var questionData = [
+    { //make sure to add real questions
+        title:"Who sang the title song for the latest Bond film, No Time to Die?",
+        choices: ["Adele","Sam Smith","Billie Eilish","David Bowi"],
+        Solution:"Billie Eilish"
+    },
+    {
+        title:"Which flies a green, white, and orange (in that order) tricolor flag? ",
+        choices: ["Ireland","Ivory Coast","Italy","Mexico"],
+        Solution:"Ireland"
+    },
+    {
+        title:"Who is generally considered the inventor of the motor car?",
+        choices: ["Henry Ford","Karl Benz","Henry M. Leland","Enzo Ferrari"],
+        Solution:"Karl Benz"
+    },
+    {
+        title:"Who was the lead singer of the band The Who?",
+        choices: ["Roger Daltrey","Don Henley","Robert Plant","Glenn Danzig"],
+        Solution:"Roger Daltrey"
+    },
+    {
+        title:"Which of the following languages has the longest alphabet?",
+        choices: ["Greek ","Russian","Arabic","Bulgarian"],
+        Solution:"Russian"
+    },
+
+]
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,22 @@
 var introEl= document.getElementById("intro")
 var questionSectionEl=document.getElementById("question-section")
 var initialInputEl= document.getElementById("intial-input")
-var highscoreEl= customElement.getElementById("highscore")
-
+var highscoreEl= document.getElementById("highscore")
 var startQuizEl= document.getElementById("start-quiz")
 var questionTitleEl= document.getElementById("question-title")
 var choiceListEl= document.getElementById("choice-list")
+var timerEl= document.getElementById("timer")
+var setIntervalId
+var timeRemaining=questionData.length * 15
 
 function startQuiz(){
+    // <section id ="intro" class="hide"> the code below represents the same thign as this line 
+    introEl.classList.add("hide")
+    questionSectionEl.classList.remove("hide")
 
+    setIntervalId=setInterval(startTimer, 1000)
+}
+function startTimer(){
+    timerEl.textContent=timeRemaining--
 }
 startQuizEl.addEventListener("click", startQuiz) //callback for click
