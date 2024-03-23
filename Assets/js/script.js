2b1b7091c563e163fe44f0f7731781ed3c0455c3
Alexander Scarlett <ascarlett92@yahoo.com>
2024-03-23 16:48:27 -0400

    added multiple quiz questions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,6 +6,9 @@ var startQuizEl= document.getElementById("start-quiz")
 var questionTitleEl= document.getElementById("question-title")
 var choiceListEl= document.getElementById("choice-list")
 var timerEl= document.getElementById("timer")
+var messageEL=document.getElementById("message")
+var scoreEl=document.getElementById("score")
+
 var setIntervalId
 var timeRemaining=questionData.length * 15
 var index=0
@@ -18,6 +21,7 @@ function startQuiz(){
     setIntervalId=setInterval(startTimer, 1000)
 }
 function renderQuestion(){
+    messageEL.innerHTML=""
     questionTitleEl.textContent=questionData[index].title
     choiceListEl.textContent=""
     //    <li><button>Choice 1</button></li>
@@ -37,16 +41,31 @@ function startTimer(){
     timerEl.textContent=timeRemaining--
 }
 function nextQuestion(event){
+    var currentChoiceBtn= event.target
+    var solution=questionData[index].solution
  index++
  if(index < questionData.length){
+     questionData
+    if(currentChoiceBtn.textContent ===solution){
+        messageEL.innerHTML="<h4>Correct!</h4>"
+    }else{
+        messageEL.innerHTML="<h4>Incorrect!</h4>"
+        timeRemaining=timeRemaining-10
+    }
+    //half second delay
+    setTimeout(renderQuestion, 500)
  renderQuestion()
 }else{
+    
     endQuiz()
 }
 }
 
 function endQuiz(){
     clearInterval(setIntervalId)
+    questionSectionEl.classList.add("hide")
+    initialInputEl.ClassList.remove("hide")
+    scoreEl.textContent=timerEl.textContent
 }
 startQuizEl.addEventListener("click", startQuiz) //callback for click
 choiceListEl.addEventListener("click", nextQuestion) //callback function for going to next question
