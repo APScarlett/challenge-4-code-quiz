e809699fffe9f00f631397a180b69e11a6ff2b14
Alexander Scarlett <ascarlett92@yahoo.com>
2024-04-29 15:33:29 -0400

    Corrected previous issues with local storage, added comments to my code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,7 @@
 var introEl= document.getElementById("intro")
 var questionSectionEl=document.getElementById("question-section")
 var initialInputEl= document.getElementById("intial-input")
+var initialEl= document.getElementById("intial")
 var highscoreEl= document.getElementById("highscore")
 var startQuizEl= document.getElementById("start-quiz")
 var questionTitleEl= document.getElementById("question-title")
@@ -11,16 +12,21 @@ var messageEL=document.getElementById("message")
 var scoreEl=document.getElementById("score")
 var goBackEL = document.getElementById("goback-btn")
 var clearScoreEL = document.getElementById("clearHighscore")
+var submitEL = document.getElementById("Submit")
+var olEl = document.getElementById("highScoreList")
 var setIntervalId
 var timeRemaining=questionData.length * 15
 var index=0
-
+var highscores= []
 function startQuiz(){
     // <section id ="intro" class="hide"> the code below represents the same thing as this line 
+   //hides the questions before they are ready to be revealed
     introEl.classList.add("hide")
+    //removes hide and allows us to see the question
     questionSectionEl.classList.remove("hide")
-
+        //starts the timer
     setIntervalId=setInterval(startTimer, 1000)
+   renderQuestion();
 }
 function renderQuestion(){
     messageEL.innerHTML=""
@@ -47,8 +53,10 @@ function startTimer(){
 function nextQuestion(event) { 
     var currentChoiceBtn = event.target;
     var solution = questionData[index].Solution; 
+   
     index++;
     if (index < questionData.length) {
+        console.log(currentChoiceBtn.textContent,solution, index, currentChoiceBtn.textContent== solution)
         if (currentChoiceBtn.textContent === solution) {
             messageEL.innerHTML = "<h4>Correct!</h4>";
         } else {
@@ -60,41 +68,57 @@ function nextQuestion(event) {
     } else {
         endQuiz();
     }
+   
 }
 
 function endQuiz(){
     clearInterval(setIntervalId)
     questionSectionEl.classList.add("hide")
-    initialInputEl.classList.remove("hide")
+    initialEl.classList.remove("hide")
     highscoreEl.classList.remove("hide")
     scoreEl.textContent=timerEl.textContent
+    displayHighscore();
+    //var highScore = localStorage.setItem("highscore", JSON.stringify(highScore))
 
-    var highScore = localStorage.setItem("highscore", JSON.stringify(highScore))
-
-  
-}
+  }
 //function to display highscore 
 function displayHighscore(){
-    clearInterval(setIntervalId)
-
-
-    var scoreList =localStorage.getItem("highscore");
+olEl.textContent=""
+    var scoreList =localStorage.getItem("highscores");
     if(scoreList !==null){
-        newList =JSON.parse(scoreList);
-        return newList;
+        highscores =JSON.parse(scoreList);
     }else{
-        newList = [];
+        highscores = [];
     }
-    return newList;
+  for(var i =0; i < highscores.length; i++){
+    var li=document.createElement("li")
+    li.textContent = highscores [i].name + "-" + highscores[i].score
+    olEl.appendChild(li)
+  }
+
 }
 
+function clearHighscore(event){
+    localStorage.clear();
+}
 
-
-
-
-startQuizEl.addEventListener("click", startQuiz) //callback for click
+startQuizEl.addEventListener("click", startQuiz) //callback for click starts the quiz
 choiceListEl.addEventListener("click", nextQuestion) //callback function for going to next question
-clearScoreEL.addEventListener("click", clearScore)
+clearScoreEL.addEventListener("click", clearHighscore) //callback to clear hs
+submitEL.addEventListener("click", submitInitials)
+function submitInitials(){
+var initials = initialInputEl.value;
+var highscore = { //object to store name/score
+    
+    "name":initials,
+    "score": timeRemaining
+};
+highscores.push(highscore)
+console.log(highscore)
+localStorage.setItem("highscores",JSON.stringify (highscores)) //local storage
+displayHighscore();
+}
+
 
 
 
